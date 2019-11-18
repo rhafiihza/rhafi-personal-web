ac881cba4d8e6d3832eb0729865804dd45ba60cd
Antonius Angga Kurniawan <antoniusanggakurniawan@gmail.com>
2019-11-19 00:31:44 +0700

    add layouts/main.blade.php and add rhafi-personal-web/index.blade.php and setup routes to index.blade.php

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="/img/logo.png" />
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="/vendor/lightbox/dist/ekko-lightbox.css">
+
+    <!-- Main CSS -->
+    <!-- Fontawesome --> <!-- Bootstrap CSS -->
+    <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Rhafi Personal Web</title>
+
+</head>
+
+<body id="page-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="/img/logo-navbar.png" alt=""
+                    style="width: 80px; height:30px ;"></a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#page-top">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#works">Works</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#section3">Skills</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    <!-- Footer -->
+    <footer class="pt-4">
+        <!-- Copyright -->
+        <div class="bg-dark text-center text-light py-3">© 2019 rhafiihza</div>
+    </footer>
+
+    
+
+
+    <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="/vendor/jquery/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    
+        <!-- Plugin JS-->
+        <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
+        <script src="/vendor/lightbox/dist/ekko-lightbox.js"></script>
+    
+        <!-- Custom JS for Scrolling when Clicked -->
+        <script src="/js/scrolling-nav.js"></script>
+        <script src="/js/lightbox.js"></script>
+</body>
+
+</html>
---
+++ resources/views/rhafi-personal-web/index.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.main')
+
+@section('content')
+    <!-- Full Page Image Header with Vertically Centered Content -->
+    <header class="masthead">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+            </div>
+        </div>
+    </header>
+    <!-- Page Content -->
+    <section class="container" id="works">
+        <h1 class="text-center mt-lg-5 mb-lg-5">Works</h1>
+        <div class="row">
+            <!-- Works 1 -->
+            <div class="col-4 mb-4">
+                <a href="" class="box4">
+                    <div class="card border-0 shadow">
+                        <img src="/img/design.png" class="card-img-top" alt="...">
+                        <div class="card-body text-center box-content">
+                            <h5 class="card-title mb-0 text-white">DESIGN</h5>
+                            <div class="card-text text-white-50">Banner</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- Works 2 -->
+            <div class="col-4 mb-4 ">
+                <a href="" class="box4">
+                    <div class="card border-0 shadow ">
+                        <img src="/img/logo.png" class="card-img-top" alt="...">
+                        <div class="card-body text-center box-content">
+                            <h5 class="card-title mb-0 text-white">DESIGN</h5>
+                            <div class="card-text text-white-50">Logo</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- Works 3 -->
+            <div class="col-4 mb-4 ">
+                <a href="" class="box4">
+                    <div class="card border-0 shadow">
+                        <img src="/img/e-commerce.png" class="card-img-top" alt="...">
+                        <div class="card-body text-center box-content">
+                            <h5 class="card-title mb-0 text-dark">WEB E-COMMERCE</h5>
+                            <div class="card-text text-black-50">Flowers Store</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.container -->
+    <!-- ABOUT -->
+    <section class="jumbotron jumbotron-fluid" id="about">
+        <div class="container text-center p-lg-5">
+            <img src="/img/6.jpg" alt="" class="rounded-circle" width="250px" height=250px">
+            <h1 class=" display-5 ">RHAFI IHZA RAJA</h1>
+            <p class=" lead ">This is a modified jumbotron that occupies the entire horizontal space of its parent.
+            </p>
+        </div>
+    </section>
+    <!-- SKILLS -->
+    <section class=" container p-lg-5">
+        <div class="row">
+            <div class="col text-center pb-4">
+                <h1 id="section3">SKILLS</h1>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col">
+                <h4>PHP NATIVE</h4>
+                <h1>0%</h1>
+            </div>
+            <div class="col">
+                <h4>LARAVEL</h4>
+                <h1>0%</h1>
+            </div>
+            <div class="col">
+                <h4>CODEIGNITER</h4>
+                <h1>0%</h1>
+            </div>
+        </div>
+    </section>
+
+    @endsection
+    
