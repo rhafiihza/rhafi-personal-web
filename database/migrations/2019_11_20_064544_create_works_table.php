f295ff6652061322ff7a738c74752f49000048f7
rhafiihza <rhafiihzaraja@gmail.com>
2019-11-26 21:12:29 +0700

    table works (icon binary type)

--- database/migrations/2019_11_20_064544_create_works_table.php
+++ database/migrations/2019_11_20_064544_create_works_table.php
@@ -15,7 +15,7 @@ class CreateWorksTable extends Migration
     {
         Schema::create('works', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->('icon');
+            $table->binary('icon');
             $table->string('title');
             $table->string('content');
             $table->timestamps();
