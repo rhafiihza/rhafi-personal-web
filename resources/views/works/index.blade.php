605e9f7a8df997f1bfe3ec428b58e4d62c921090
rhafiihza <rhafiihzaraja@gmail.com>
2019-12-06 17:19:15 +0700

    changed skills section style

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -1,67 +1,76 @@
 @extends('layouts.main')
 
 @section('content')
-    <!-- Full Page Image Header with Vertically Centered Content -->
-    <header class="masthead">
-        
-        <div class="container h-100">
-            <div class="row h-100 align-items-center">
-            </div>
+<!-- Full Page Image Header with Vertically Centered Content -->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
         </div>
-    </header>
-    <!-- Page Content -->
-    <section class="container" id="works">
-        <h1 class="text-center mt-lg-5 mb-lg-5">Works</h1>
-        <div class="row">
+    </div>
+</header>
+<!-- Page Content -->
+<!-- Works -->
+<section class="container" id="works">
+    <h1 class="text-center mt-lg-5 mb-lg-5">Works</h1>
+    <div class="row">
         @foreach($works as $data)
-            <!-- Works 1 -->
-            <div class="col-4 mb-4">
-                <a href="" class="box4">
-                    <div class="card border-0 shadow">
-                        <img src="{{ $data->icon }}" class="card-img-top" alt="...">
-                        <div class="card-body text-center box-content">
-                            <h5 class="card-title mb-0 text-white bg-info">{{ $data->title }}</h5>
-                            <div class="card-text text-white-50 bg-info">{{ $data->content }}</div>
-                        </div>
+        <div class="col-4 mb-4">
+            <a href="" class="box4">
+                <div class="card border-0 shadow">
+                    <img src="{{ $data->icon }}" class="card-img-top" alt="...">
+                    <div class="card-body text-center box-content">
+                        <h5 class="card-title mb-0 text-white bg-info">{{ $data->title }}</h5>
+                        <div class="card-text text-white-50 bg-info">{{ $data->content }}</div>
                     </div>
-                </a>
-            </div>
-            @endforeach
+                </div>
+            </a>
         </div>
-        <!-- /.row -->
-    </section>
-    <!-- /.container -->
-    <!-- ABOUT -->
-    <section class="jumbotron jumbotron-fluid" id="about">
-        <div class="container text-center p-lg-5">
-            <img src="{{asset('img/6.jpg')}}" alt="" class="rounded-circle" width="250px" height=250px">
-            <h1 class=" display-5 ">RHAFI IHZA RAJA</h1>
-            <p class=" lead ">This is a modified jumbotron that occupies the entire horizontal space of its parent.
-            </p>
+        @endforeach
+    </div>
+    <!-- /.row -->
+</section>
+<!-- /.Works -->
+<!-- /.container -->
+<!-- ABOUT -->
+<section class="jumbotron jumbotron-fluid" id="about">
+    <h1 class="text-center">About</h1>
+    <div class="container text-center p-lg-5">
+        <img src="{{asset('img/profile.jpg')}}" alt="" class="rounded-circle" width="200px" height=250px">
+        <h1 class=" display-5 ">RHAFI IHZA RAJA</h1>
+        <p class=" lead ">This is a modified jumbotron that occupies the entire horizontal space of its parent.
+        </p>
+    </div>
+</section>
+<!--/.SECTION ABOUT-->
+<!-- SKILLS -->
+<section class="container" id="skills">
+            <h1 id="section3" class="text-center mb-lg-5">SKILLS</h1>
+    <div class="flex-wrapper">
+        <div class="single-chart">
+            <h4 class="text-center">CodeIgniter</h4>
+            <svg viewBox="0 0 36 36" class="circular-chart orange">
+                <path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
+                <path class="circle" stroke-dasharray="30, 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
+                <text x="18" y="20.35" class="percentage">30%</text>
+            </svg>
         </div>
-    </section>
-    <!-- SKILLS -->
-    <section class=" container p-lg-5">
-        <div class="row">
-            <div class="col text-center pb-4">
-                <h1 id="section3">SKILLS</h1>
-            </div>
+        <div class="single-chart">
+            <h4 class="text-center">Laravel</h4>
+            <svg viewBox="0 0 36 36" class="circular-chart green">
+                <path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
+                <path class="circle" stroke-dasharray="60, 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
+                <text x="18" y="20.35" class="percentage">60%</text>
+            </svg>
         </div>
-        <div class="row text-center">
-            <div class="col">
-                <h4>PHP NATIVE</h4>
-                <h1>0%</h1>
-            </div>
-            <div class="col">
-                <h4>LARAVEL</h4>
-                <h1>0%</h1>
-            </div>
-            <div class="col">
-                <h4>CODEIGNITER</h4>
-                <h1>0%</h1>
-            </div>
+        <div class="single-chart">
+            <h3 class="text-center">Native PHP</h3>
+            <svg viewBox="0 0 36 36" class="circular-chart blue">
+                <path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
+                <path class="circle" stroke-dasharray="90, 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" />
+                <text x="18" y="20.35" class="percentage">90%</text>
+            </svg>
         </div>
-    </section>
-
-    @endsection
-    
+    </div>
+</section>
+<!--/.SECTION SKILLS-->
+@endsection
